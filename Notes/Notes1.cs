393c2e246b1d843ddfff80882493bff3c4bc3f69
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-06 10:34:26 +0200

    C# Notes
    Just taking some notes

---
+++ Notes/Notes1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace New_folder
+{
+    class Program
+    {
+        static int x = 0, y = 5;
+
+        static void Main(string[] args)
+        {
+            //These two functions do the same thing
+            WhileTest();
+            ForTest();
+        }
+
+        static void WhileTest () {
+            //Simple while example
+            while(x < y)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+
+        static void ForTest () {
+            /*Initializaiton; Condition; Steps
+            Declare and initialize var; Execute code til condition is true/false; Increment to keep progressing code*/
+            for (int i = 0; i < y; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
